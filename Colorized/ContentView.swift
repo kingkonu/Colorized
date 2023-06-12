c6e715e9fff39b13a033785f522304facca3b265
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-06-12 19:59:52 +0300

    Create Slider

--- Colorized/ContentView.swift
+++ Colorized/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 1...255)
+    @State private var greenSliderValue = Double.random(in: 1...255)
+    @State private var blueSliderValue = Double.random(in: 1...255)
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(lround(redSliderValue))")
+            ColorSliderView(value: $redSliderValue, color: .red)
+            ColorSliderView(value: $greenSliderValue, color: .green)
+            ColorSliderView(value: $blueSliderValue, color: .blue)
         }
         .padding()
     }
@@ -22,5 +26,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+
+    let color: Color
+
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(color)
+            Slider(value: $value, in: 1...255, step: 1).accentColor(color)
+            Text("255").foregroundColor(color)
+        }
     }
 }
