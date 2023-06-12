1d3207f7b059d47ad439e6e646b729ccdb16265d
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-06-12 15:05:39 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Alexey Kanaev on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
